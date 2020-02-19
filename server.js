98345f239c99edd52cb6b59cdefcf66a877a6be6
disko998 <disko998@gmail.com>
2020-02-19 03:07:28 +0100

    splitting up the code

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
 
+// Route file
+const bootcamps = require('./routes/bootcamps')
+
+// Load env vars
 dotenv.config({ path: './config/config.env' })
 
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+// Mount routes
+app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
